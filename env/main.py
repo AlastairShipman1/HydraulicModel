5b453410dbbef96d732eff64c0d06827d96f67f3
Alastair Shipman <alastair.shipman@gmail.com>
2019-11-05 14:53:25 +0000

    now need to do a read through of the book. think the set up is ok, but need to check the theory. should probably have done this first

--- env/main.py
+++ env/main.py
@@ -12,25 +12,86 @@ need to make sure we can understand dominance of staircases over doors?
 
 '''
 
+
+'''
+The hydraulic model (is this first order or second order?)
+
+1) Set up a group of people, defined by number, density and location (an element).
+2) They then start moving towards the first transition (e.g. a door to another room), which leads to the next element
+3) Their movement speed is determined by the location and density, the time to transition is determined by the speed.
+4) Once at the transition, we need to understand the density on both sides of the transition, which is found by solving a quadratic
+4.1) You know the flow rate into the transition, and using F=kD(1-aD)We, we can solve for D
+5) Once you have D on the otherside of the transition, you can find S=k-akD, and therefore the time to traverse the next element.
+6) If the flow rate is limited (by a door, or the Fmax of the element), then the max flow rate is limited. 
+6.1) If the flow rate through a transition is lower than through the element, then queuing occurs. The length of this queue needs to be tracked
+
+'''
+
+
+class Person():
+    def __init__(self, element):
+        self.current_element=element
+        self.time_in_element=0
+
+    def step_time(self):
+        self.time_in_element+=1
+
+    def change_element(self, element):
+        self.current_element=element
+        self.time_in_element=0
+
+
+class Transition():
+    'used for doors, entry to staircases, etc'
+    boundary_layers = {'Stairs': 0.15, 'Handrail': 0.98, 'Chair': 0, 'Corridor': 0.2, 'Obstacles': 0.1,
+                       'Concourse': 0.46, 'Door': 0.15}
+
+    def __init__(self, width, boundary_layer1=None, boundary_layer2=None):
+        self.width=width-boundary_layers[boundary_layer1]-boundary_layers[boundary_layer2]
+        self.max_speed = 1.19  # m/s
+        self.flow_rate = 1.3 * self.width  # ppl/s/m per unit effective width * effective width... unless a staircase!
+        self.k = 1.4
+        self.a = 0.266
+class Door(Transition):
+    'stipulate width, type, etc'
+
+    def __init__(self, held_open=False):
+        if held_open:
+            self.max_flow_rate=50
+class StaircaseEntry(Transition):
+    'entry to a staircase element'
+    def __init__(self, tread, riser):
+        self.tread = tread
+        self.riser = riser
+        self.max_speed = 0.941 - 0.066667 * self.riser + 0.0416667 * self.tread  # values from regression performed on data
+        self.k = 0.45 - 0.2 * self.riser + 0.07 * self.tread  # same again
+        self.max_flow_rate = 0.271667 - 0.006667 * self.riser + 0.071667 * self.tread  # and here
+
+
 class Element():
     #placeholder class for your destinations
     max_flows={}
     boundary_layers={'Stairs':0.15 , 'Handrail':0.98, 'Chair':0, 'Corridor':0.2, 'Obstacles':0.1, 'Concourse':0.46,'Door':0.15}
 
-    def __init__(self, length, width, element_type, population, global_timer:Global_timer, boundary_layer1=None, boundary_layer2=None, tread=None, riser=None):
+    def __init__(self, name, length, width, element_type, population, global_logger:Global_logger, global_timer:Global_timer, boundary_layer1=None, boundary_layer2=None, tread=None, riser=None):
+        self.name=name
         self.length=length
         self.width=width-boundary_layers[boundary_layer1]-boundary_layers[boundary_layer2] #this is effective width. will it being Nonetype present an issue?
         self.area=self.length*self.width
         self.type=element_type
         self.global_timer=global_timer
+        self.global_logger=global_logger
         self.time = 0
 
+        # it would be easy enough to change this to a population of agents, each a class of person, with travel speed etc.
         self.population = population
         self.max_population = np.inf  # we should define the max population density, and then calculate the max element population
         self.outflow=0
         self.inflow=0
+        # here we have to define the max inflow and outflow of each element.
         self.max_inflow=np.inf
         self.max_outflow=np.inf
+
         self.possible_queuing=False
         self.queuing=False
         self.queue_length=0
@@ -48,22 +109,27 @@ class Element():
             self.k=0.45-0.2*self.riser+0.07*self.tread #same again
             self.max_flow_rate=0.271667-0.006667*self.riser+0.071667*self.tread #and here
 
-    def define_entrypoint(self, entrypoint:Destination):
+    def define_entrypoint(self, entry_element:Element, entry_transition: Transition):
         #initially have only one entrypoint. could extend this to multiple fairly easily?
-        self.entrypoint=entrypoint
+        self.entry_element=entry_element
+        self.entry_transition=entry_transition
 
-    def define_exitpoint (self, exitpoint:Destination):
+    def define_exitpoint (self, exit_element:Element, exit_transition:Transition):
         #initially have only one exitpoint. may need to extend this if there is queuing?
-        self.exitpoint=exitpoint
+        self.exit_element=exit_element
+        self.exit_transition=exit_transition
 
     def step_time(self):
         self.check_global_time()# we need to do something with this- either slow everything else down, or step through everything again
         self.check_current_status()
+        self.calc_density_then_velocity()
         self.check_inflows()
         self.check_outflows()
 #        self.refresh_variables()
         self.time+=1
         self.initial_flow_timer+=1
+        self.check_queuing()
+
 
     def check_current_status(self):
         'here we need to check the element has people are queuing, walking or is empty'
@@ -114,29 +180,51 @@ class Element():
         here we also have to check if queuing is occurring, and also the limitation on exit flow rates
         '''
 
+        # if queuing not possible, outflow must be zero.
+
         if self.exitpoint.population==self.exitpoint.max_population:
             self.outflow=0
         else:
-            self.flow_rate=self.calculate_predicted_flow()
-            '''
-            need to implement the flow rate, however they do that
-            '''
-            self.outflow=min(self.max_flow_rate, self.flow_rate, self.exitpoint.max_inflow)
-            if self.outflow<self.flow_rate:
-                self.queue_length+=self.flow_rate-self.outflow
-                self.population-= self.outflow
+            if not self.possible_queuing:
+                self.outflow=0
+            else:
+                self.flow_rate=self.calculate_predicted_flow()
+                '''
+                need to implement the flow rate, however they do that
+                '''
+                self.outflow=min(self.max_flow_rate, self.flow_rate, self.exitpoint.max_inflow)
+                if self.outflow<self.flow_rate:
+                    self.queue_length+=self.flow_rate-self.outflow
+                    self.population-= self.outflow
+                    if self.population==0:
+                        self.possible_queuing= False
+                        self.global_logger.add_entry((self.name, self.global_timer.global_time))
+                        # here log the time that this element has emptied.
 
     def calculate_predicted_flow(self):
         'solve the quadratic using density'
-        'this will give Fs'
-        return self.velocity*self.area # this isn't right, is it?
+        'this will give F=kD(1-aD)*We'
+        calculated_flow=self.k*self.density(1-self.a*(self.density**2))*self.width
+        return calculated_flow
 
-    def move_through_element(self):
+    def calculate_transition_outward_flow(self):
+        internal_flow=self.calculate_predicted_flow()
+        return internal_flow*self.width/self.exitpoint.width
+
+    def calculate_post_transition_density_inward_flow(self):
+        a=
+        b=
+        c=
+        x1=(-b -np.sqrt(b**2-4*a*c))/(2*a)
+        x2=(-b +np.sqrt(b**2-4*a*c))/(2*a)
+        self.density=min(x1, x2, self.max_flow_rate)
+        return min(x1, x2)
+
+    def check_queuing(self):
         'this will add to the queues'
         'the time to go from entrypoint to exit point, depending on speed (flow rate)'
         if self.initial_flow_timer > self.unimpeded_flow_time:
             self.possible_queuing=True
-
 
 class Outdoors (Element):
     def __init__(self):
@@ -161,7 +249,6 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
 class Global_timer(metaclass=Singleton):
 
     def __init__(self):
@@ -169,22 +256,40 @@ class Global_timer(metaclass=Singleton):
 
     def step_time(self):
         self.global_time+=1
+class Global_logger(metaclass=Singleton):
+    def __init__(self):
+        self.log=[]
 
-def step_time(environment, global_timer, timestep):
-    global_timer.step_time()
+    def add_entry(self, entry):
+        self.log.append(entry)
 
-    # here we need to randomise the order of the elements in the environment as we cycle through them
-    # otherwise, if there is a blockage, one element will never actually evacuate until the other elements have emptied their queues
-    # this is only an issue if you have multiple entrypoints
-    # and is not an issue if you define where the flows will be coming from (e.g. staircase empties floor by floor)
-    for element in environment:
-        element.step_time()
 
+def step_time(environment, global_timer, global_logger, timestep):
+    for i in range(3):#there are people in the building):
+        global_timer.step_time()
+        # here we need to randomise the order of the elements in the environment as we cycle through them
+        # otherwise, if there is a blockage, one element will never actually evacuate until the other elements have emptied their queues
+        # this is only an issue if you have multiple entrypoints
+        # and is not an issue if you define where the flows will be coming from (e.g. staircase empties floor by floor)
+        for element in environment:
+            element.step_time()
+    print(global_logger.log)
+
+
+
+'''
+so we start by defining the environment, and the density of the group of people.
+then we start them off on the evacuation route.
+'''
+gl=Global_logger()
+gt=Global_timer()
 environment=[]
+
 room=Room(10, 10, 100)
 stairs=Staircase(10, 10, 0, 1, 1)
 corridor=Corridor(20, 2, 0)
 door=Door()
+
 outdoors=Outdoors()
 
 room.define_exitpoint(stairs)
