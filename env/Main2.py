feaee2805477bb099f870e93c35afef1f5efaa5f
Alastair Shipman <alastair.shipman@gmail.com>
2019-11-11 12:19:46 +0000

    the queue system isn't working... try changing the position of back of queue

--- env/Main2.py
+++ env/Main2.py
@@ -47,45 +47,30 @@ class Global_logger(metaclass=Singleton):
         self.log.append(entry)
 
 class Element():
-    #placeholder class for your elements, keeps track of how many people are in each element, and when they enter/leave/queue
-    class Population_tracker():
-        def __init__(self, population):
-            self.queue_length=0
-            self.population=population
-            self.unimpeded_traversal_time=self.length/self.max_speed
-            self.flow_timer=0
-            self.possible_queuing = False
-            self.queuing = False
-            #need to implement a queue for the population, to make sure that it is first in first out.
-            #self.people = deque
-            self.max_population = np.inf  # we should define the max population density, and then calculate the max element population
-
-        def increment_flow_timer(self):
-            self.flow_timer+=config.timestep
-
-
-
 
     def __init__(self, name, length, width, element_type, population, global_timer:Global_timer, boundary_layer1=None, boundary_layer2=None, tread=None, riser=None):
         boundary_layers = {'Stairs': 0.15, 'Handrail': 0.98, 'Chair': 0, 'Corridor': 0.2, 'Obstacles': 0.1,
                            'Concourse': 0.46, 'Door': 0.15}
-
         self.name=name
         self.length=length
         self.width=width-boundary_layers[boundary_layer1]-boundary_layers[boundary_layer2] #this is effective width. will it being Nonetype present an issue?
         self.area=self.length*self.width
         self.type=element_type
+        self.inflow_transition=None
+        self.outflow_transition=None
 
         self.global_timer=global_timer
         self.time = 0
 
         self.outflow=0
         self.inflow=0
+        self.calc_flow=0
         # here we have to define the max inflow and outflow of each element.
         self.max_inflow=np.inf
         self.max_outflow=np.inf
 
-        self.population_tracker=self.Population_tracker(population)
+        self.position_of_front=0
+        self.position_of_back=0
 
         '''
         This updates in fractional seconds, defined by config.timestep
@@ -105,38 +90,72 @@ class Element():
             self.k=0.45-0.2*self.riser+0.07*self.tread #same again
             self.max_flow_rate=0.271667-0.006667*self.riser+0.071667*self.tread *config.timestep #and here
 
+        self.queue_length = 0
+        self.population = population
+        self.unimpeded_traversal_time = self.length / self.max_speed
+        self.flow_timer = 0
+        self.possible_queuing = False
+        self.queuing = False
+
+        # need to implement a queue for the population, to make sure that it is first in first out.
+        # self.people = deque
+        self.max_population = np.inf  # we should define the max population density, and then calculate the max element population
         self.set_speed()
         self.set_traversal_time()
 
+    def increment_flow_timer(self):
+        self.flow_timer += config.timestep
     #implement basic functions for speed, density, flow rate
     def step_time(self):
         'each timestep, we need to check how many people are coming into the element, how many people are leaving, whether people have got to point of leaving'
         ' if there is a queue, whether it decreases in length or increases in length'
-        self.calc_current_flow_rate()
-        pop_entering=self.check_people_are_entering()
-        pop_exiting=self.check_people_are_exiting()
 
-        if pop_exiting:
+        if self.population>0:
+            self.calc_current_flow_rate()
+            self.check_front_of_group()
+            self.check_back_of_group()
+
+        pop_entering=self.check_people_are_entering()
+
+
+        if self.position_of_front>0.95 and self.population>0:
             self.set_outflow()
         if pop_entering:
             self.set_inflow_rate()
             self.set_density_given_inflow()
 
+
         self.update_population_tracker()
         self.time+=config.timestep
+        if self.population>0:
+            self.flow_timer += config.timestep
+
+    def check_front_of_group(self):
+        #in a proportion of the length of the element.
+        self.position_of_front=min(1, (self.speed*self.flow_timer)/self.length)
+        if self.position_of_front>0.95:
+            self.possible_queuing=True
+
+    def check_back_of_group(self):
+        group_length=min((self.width*self.population)/(self.density*self.length), 5)#proportion of length of the element
+        self.position_of_back=max(0, self.position_of_front-group_length)
+
 
     def check_people_are_exiting(self):
         'check people are in the element, and they have got to the exit point. if they are, then self.possible_queueing=True, and return true'
-        if self.population_tracker.flow_timer>self.population_tracker.unimpeded_traversal_time:
-            self.population_tracker.possible_queuing=True
-        if self.population_tracker.possible_queuing:
+        if self.flow_timer>self.unimpeded_traversal_time:
+            self.possible_queuing=True
+        if self.possible_queuing:
             return True
         else:
             return False
 
     def check_people_are_entering(self):
         'check people in the entry point are ready to enter, if they are, then return true'
-        return self.inflow_point.population_tracker.possible_queuing
+        if self.inflow_point==self:
+            return False
+        else:
+            return self.inflow_point.possible_queuing
 
     def get_speed(self):
         return self.speed
@@ -177,62 +196,89 @@ class Element():
         self.calc_flow_rate()
         return self.calc_flow
 
-    def set_inflow_point(self, inflow_point):
+    def set_inflow_point(self, inflow_point, inflow_transition=None):
+        if inflow_point==self:
+            self.inflow=0
         self.inflow_point=inflow_point
+        if inflow_transition is not None:
+            self.inflow_transition=inflow_transition
 
-    def set_outflow_point(self, outflow_point):
+    def set_outflow_point(self, outflow_point, outflow_transition=None):
         self.outflow_point=outflow_point
+        if outflow_transition is not None:
+            self.outflow_transition=outflow_transition
 
     def get_inflow_rate(self):
         return self.inflow
 
     def set_inflow_rate(self):
         inflow_element=self.inflow_point
-        self.inflow=min(self.max_flow_rate, inflow_element.outflow)
+        if inflow_element.population==0:
+            self.inflow=0
+            return
+        if self.inflow_transition:
+            self.inflow=min(self.max_flow_rate, self.inflow_transition.max_flow_rate, inflow_element.outflow)
+        elif self.inflow_point.name==self.name:
+            # it turns out this is inaccessible if we code properly, as this function will never be called for a
+            # population source.
+            self.inflow=0
+        else:
+            self.inflow=min(self.max_flow_rate, inflow_element.outflow)
 
     def set_outflow(self):
         outflow_element=self.outflow_point
-        if self.population>0 and self.possible_queuing:
-            self.outflow=min(self.calc_flow, outflow_element.max_flow_rate)
+        if self.outflow_transition is not None:
+            self.outflow=min(self.calc_flow, self.outflow_transition.max_flow_rate, outflow_element.max_flow_rate)
+        else:
+            self.outflow = min(self.calc_flow, outflow_element.max_flow_rate)
 
     def get_outflow(self):
         return self.outflow
 
     def update_population_tracker(self):
         ' here we access a class called population tracker, which checks the length of the queue relative to population etc'
-        self.population_tracker.population+=self.inflow
-        self.population_tracker.population-=self.outflow
-        self.population_tracker.increment_flow_timer()
-        if self.population_tracker.population<0:
-            self.population_tracker.population=0
-            print(self.name+ " has emptied at: "+ self.global_timer.global_time+ "s")
-        if self.population_tracker.queue_length > 0:
-            self.population_tracker.queue_length -= self.outflow
-            if self.population_tracker.queue_length < 0:
-                self.possible_queuing = False
-                self.population_tracker.queue_length = 0
+        self.population+=self.inflow
+        self.population-=self.outflow
+        self.increment_flow_timer()
+        if self.population<0 and self.position_of_back>0.95:
+            self.population=0
+            self.outflow=0
+            self.position_of_back==0
+            self.flow_timer=0
 
+
+        #these two can be combined.. think about this later
+        #here we check the queue length, and edit it
+        if self.queue_length > 0:
+            self.queue_length -= self.outflow
+            if self.queue_length < 0:
+                self.possible_queuing = False
+                self.queue_length = 0
+
+        #here we initialise the queues
         if self.calc_flow > self.outflow_point.max_flow_rate:
             self.possible_queuing = True
             if self.calc_flow > 0:
-                self.population_tracker.queue_length += (self.calc_flow-self.outflow)
+                self.queue_length += (self.calc_flow-self.outflow)
+
+class Transition():
+    def __init__(self, max_flow_rate):
+        self.max_flow_rate=max_flow_rate
 
 class Outdoors():
     def __init__(self):
         ' we need to ensure that all the required variables from elements exitpoints are covered here. how to future proof this?'
         self.width=np.inf
         self.length=np.inf
-        self.max_flow=np.inf
+        self.max_flow_rate=np.inf
         self.area=np.inf
         self.population=0 # done here to ensure the density is always zero
 class Corridor(Element):
     'also aisle, ramp, doorway'
 class Staircase(Element):
     'check'
-class Door(Element):
+class Door(Transition):
     'check'
-
-
 
 def step_time(environment, global_timer):
     global_timer.step_time()
@@ -240,8 +286,14 @@ def step_time(environment, global_timer):
     # otherwise, if there is a blockage, one element will never actually evacuate until the other elements have emptied their queues
     # this is only an issue if you have multiple entrypoints
     # and is not an issue if you define where the flows will be coming from (e.g. staircase empties floor by floor)
+    total_population=0
     for element in environment:
         element.step_time()
+        print(element.name, element.population, element.outflow)
+        print(element.name, element.position_of_front, element.position_of_back)
+        total_population+=element.population
+#    print(total_population)
+
 
 '''
 so we start by defining the environment, and the density of the group of people.
@@ -250,17 +302,38 @@ then we start them off on the evacuation route.
 environment=[]
 global_timer=Global_timer()
 
-stairs=Staircase(name='stairs', length=3.31, width=1.8, element_type='Stairs', population=50, global_timer=global_timer, boundary_layer1='Stairs', boundary_layer2='Stairs', tread=10, riser=7)
+#stairs=Staircase(name='stairs', length=3.31, width=1.8, element_type='Stairs', population=50, global_timer=global_timer, boundary_layer1='Stairs', boundary_layer2='Stairs', tread=10, riser=7)
 corridor=Corridor(name='corridor', length=10, width=1.8, element_type='Corridor', population=50, global_timer=global_timer, boundary_layer1='Corridor', boundary_layer2='Corridor')
-door=Door()
+corridor2=Corridor(name='corridor2', length=10, width=1.8, element_type='Corridor', population=0, global_timer=global_timer, boundary_layer1='Corridor', boundary_layer2='Corridor')
+corridor3=Corridor(name='corridor3', length=10, width=1.8, element_type='Corridor', population=0, global_timer=global_timer, boundary_layer1='Corridor', boundary_layer2='Corridor')
+corridor4=Corridor(name='corridor4', length=10, width=1.8, element_type='Corridor', population=0, global_timer=global_timer, boundary_layer1='Corridor', boundary_layer2='Corridor')
+
+
+door=Door(np.inf)
+door2=Door(np.inf)
+door3=Door(np.inf)
+door4=Door(np.inf)
 outdoors=Outdoors()
 
 
-stairs.define_exitpoint(corridor)
-corridor.define_entrypoint(stairs)
-corridor.define_exitpoint(door)
-door.define_entrypoint(corridor)
-door.define_exitpoint(outdoors)
+corridor.set_inflow_point(corridor)
+corridor.set_outflow_point(corridor2, door)
+corridor.set_initial_density(1)
 
-environment.append(room, stairs, corridor, door)
+corridor2.set_inflow_point(corridor, door2)
+corridor2.set_outflow_point(corridor3, door2)
+
+corridor3.set_inflow_point(corridor2, door2)
+corridor3.set_outflow_point(outdoors, door3)
+
+corridor4.set_inflow_point(corridor3, door4)
+corridor4.set_outflow_point(outdoors, door4)
+
+environment.append(corridor)
+environment.append(corridor2)
+environment.append(corridor3)
+environment.append(corridor4)
+
+for i in range(1000):
+    step_time(environment, global_timer)
 
